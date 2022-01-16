a12b3e6f3fccda7432f8c17d051dfb1f631d0498
avilive <ivanyukaleksandr@gmail.com>
2022-01-16 06:46:28 +0200

    1409 1877 2001

---
+++ 1409/acm_timus_1409/acm_timus_1409/acm_timus_1409.cpp
@@ -0,0 +1,17 @@
+﻿
+
+#include <iostream>
+
+
+
+int main()
+{
+    int n1, n2; // amount of cans shot
+    int m1, m2; // amount of cans missed
+    std::cin >> n1 >> n2;
+    m1 = (n1 + n2 - 2) - n2 + 1;
+    m2= (n1 + n2 - 2) - n1 + 1;
+
+    std::cout << m2<<' '<<m1 << std::endl;
+}
+
